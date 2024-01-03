a720c0b1eefac80a3b88e6fe792cccd8d338993f
carol <129760489+carolgarr@users.noreply.github.com>
2024-01-03 19:32:07 +0000

    pgrel table created and finished

--- docker/images/db/rel/initial_schema.sql
+++ docker/images/db/rel/initial_schema.sql
@@ -2,49 +2,32 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS POSTGIS;
 CREATE EXTENSION IF NOT EXISTS POSTGIS_TOPOLOGY;
 
-CREATE TABLE public.teams (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) NOT NULL,
+CREATE TABLE public.sightings (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v5(),
+	ufo_shape_ref UUID NOT NULL,
+    date_encounter DATE,
+    time_encounter TIME,
+    season_encounter VARCHAR(250),
+    date_documented DATE,
+    country VARCHAR(250),
+    region VARCHAR(250),
+    locale VARCHAR(250),
+    location_geometry GEOMETRY(Point, 4326), /*4326 represents the SRID (Spatial Reference ID) for WGS 84, which is commonly used for GPS coordinates. so that we can know that point has longitude and latitude*/
+    encounter_duration_text VARCHAR(255),
+    encounter_duration_seconds INTEGER,
+    description TEXT
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE public.countries (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) UNIQUE NOT NULL,
-	geom            GEOMETRY,
+CREATE TABLE public.ufo_shapes (
+	id UUID PRIMARY KEY DEFAULT uuid_generate_v5(),
+    shape_name VARCHAR(255) NOT NULL
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE public.players (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) NOT NULL,
-	age             INT NOT NULL,
-	team_id         uuid,
-	country_id      uuid NOT NULL,
-	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-ALTER TABLE players
-    ADD CONSTRAINT players_countries_id_fk
-        FOREIGN KEY (country_id) REFERENCES countries
-            ON DELETE CASCADE;
-
-ALTER TABLE players
-    ADD CONSTRAINT players_teams_id_fk
-        FOREIGN KEY (team_id) REFERENCES teams
+ALTER TABLE public.sightings
+    ADD CONSTRAINT sightings_ufo_shapes_fk
+        FOREIGN KEY (ufo_shape_ref) REFERENCES public.ufo_shapes(id)
             ON DELETE SET NULL;
-
-/* Sample table and data that we can insert once the database is created for the first time */
-CREATE TABLE public.teachers (
-	name    VARCHAR (100),
-	city    VARCHAR(100),
-	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-INSERT INTO teachers(name, city) VALUES('Luís Teófilo', 'Porto');
-INSERT INTO teachers(name, city) VALUES('Ricardo Castro', 'Braga');
-
